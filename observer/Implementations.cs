4ddd0a6ef83f73261220ebafe0271f791e099dad
Perry Chen <2558544545qwerty@gmail.com>
2024-08-21 14:34:14 +0300

    Feat: completed implementation for observers

--- observer/Implementations.cs
+++ observer/Implementations.cs
@@ -72,6 +72,6 @@ public class TicketStockService : ITicketChangeListener
 {
     public void ReceiveTicketChangeNotification(TicketChange ticketChange)
     {
-        Console.WriteLine($"{nameof(TicketResellerService)} notified of ticket change: artist {ticketChange.ArtistId}, amount ${ticketChange.Amount}");
+        Console.WriteLine($"{nameof(TicketStockService)} notified of ticket change: artist {ticketChange.ArtistId}, amount ${ticketChange.Amount}");
     }
 }
--- observer/Program.cs
+++ observer/Program.cs
@@ -4,6 +4,18 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        Console.Title = "Observer";
+        OrderService orderService = new();
+        TicketResellerService ticketResellerService = new();
+        TicketStockService ticketStockService = new();
+
+        orderService.AddObserver(ticketStockService);
+        orderService.AddObserver(ticketResellerService);
+
+        orderService.CompleteTicketSale(1, 200);
+
+        orderService.CompleteTicketSale(2, 400);
+
+        Console.ReadKey();
     }
 }
